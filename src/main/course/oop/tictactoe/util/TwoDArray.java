88c3e451c1e5ebed9c8c6d70fdaad3e2c6397d4a
aaron.williams1297@gmail.com <Aaron.williams1297@gmail.com>
2019-01-20 14:21:51 -0500

    Added functionality of methods

--- src/main/course/oop/tictactoe/util/TwoDArray.java
+++ src/main/course/oop/tictactoe/util/TwoDArray.java
@@ -1,4 +1,6 @@
 package main.course.oop.tictactoe.util;
+import java.lang.StringBuilder;
+import java.util.Stack;
 
 /**
  * You must implement the following methods to accept the expected 
@@ -10,18 +12,47 @@ package main.course.oop.tictactoe.util;
  */
 public class TwoDArray {
 	
+	private int defaultValue;
+	private int arrayColumns;
+	private int arrayRows;
+	private int[][] array;
+	
 	
 	public TwoDArray(int rows, int cols, int defaultVal){
 		/*TODO - Create a 2D integer array consisting of 
 		 * the number of rows and columns given. Initialize 
 		 * the array by setting each int to be the defaulVal. 
 		 * */
+		
+		defaultValue = defaultVal;
+		arrayColumns = cols;
+		arrayRows = rows;
+		
+		array = new int[rows][cols];
+		for (int i=0; i<rows; i++) 
+		{
+			for(int j=0; j<cols; j++) 
+			{
+				array[i][j] = defaultVal;
+			}
+		}
+		
+		
+		
 	}
 	
 	public void initArray(int defaultVal) {
 		/*TODO - (Re)Initialize the array by 
 		 * setting each int to be the defaulVal 
 		 */
+		defaultValue = defaultVal;
+		for (int i=0; i<arrayRows; i++) 
+		{
+			for(int j=0; j<arrayColumns; j++) 
+			{
+				array[i][j] = defaultVal;
+			}
+		}
 		
 	}
 	
@@ -39,6 +70,23 @@ public class TwoDArray {
 		 * Note: Print the int value in place of (). 
 		 * e.g., replace (val) with val.
 		 */
+		
+		if (val ==defaultValue) 
+		{
+			return "Failure: " + val+ " is not allowed.";
+		}
+		
+		if (array[row][col] == defaultValue) 
+		{
+			array[row][col] = val;
+			return "Success! " + val +" was inserted.";
+		}
+		
+		if (array[row][col] != defaultValue) 
+		{
+			return "Failure: " + row + ", " + col + " is not empty.";
+		}
+		
 		return "Not implemented";
 	}
 	
@@ -46,8 +94,8 @@ public class TwoDArray {
 		/*TODO - Return the value at the specified row, col
 		 * 
 		 */
+		return array[row][col];
 		
-		return 0;
 	}
 	
 	public String getArrayDisplay() {
@@ -58,8 +106,17 @@ public class TwoDArray {
 		 *  0	1	1
 		 * 
 		 */
+		StringBuilder toReturn = new StringBuilder();
+		toReturn.append("\n");
+		for (int i =0; i<arrayRows; i++) {
+			for (int j=0; j<arrayColumns; j++) {
+				toReturn = toReturn.append(array[i][j]);
+				toReturn = toReturn.append(' ');
+			}
+			toReturn = toReturn.append("\n");
+		}
 		
-		return "Not implemented";
+		return toReturn.toString();
 	}
 	
 	public String getArrayDetails() {
@@ -75,7 +132,48 @@ public class TwoDArray {
 		 * 
 		 */
 		
-		return "Not implemented";
+		StringBuilder toReturn = new StringBuilder();
+		
+		toReturn.append(arrayRows);
+		toReturn.append("\n");
+		toReturn.append(arrayColumns);
+		toReturn.append("\n");
+		
+		Stack<Integer> temp = new Stack<Integer>();
+		
+		for (int i=0; i<arrayRows; i++) 
+		{
+			for(int j=0; j<arrayColumns; j++) 
+			{
+				if(!temp.contains(array[i][j])) {
+					temp.push(array[i][j]);
+				}
+			}
+		}
+		toReturn.append(temp.size());
+		toReturn.append("\n");
+		
+		
+		while(!temp.isEmpty()) {
+			
+			int count = 0;
+			int compare= temp.pop();
+			
+			for(int i =0; i< arrayRows; i++) {
+				for (int j=0; j<arrayColumns; j++) {
+					if (array[i][j] == compare) {
+						count++;
+					}
+				}
+			}
+			
+			toReturn.append("value: "+ compare + " count: " + count + "\n");
+			
+			
+		}
+		
+		
+		return toReturn.toString();
 	}		
 
 }
